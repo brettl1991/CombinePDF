7b5814f8bc2ece9ad3ddf36067a8d2b59e9ebb43
Agnes <brettl1991@gmail.com>
2021-04-15 14:53:29 +0100

    Create merge obj and merge everyting into it the pdf files

--- combinePDF.py
+++ combinePDF.py
@@ -5,7 +5,10 @@ import sys
 inputs = sys.argv[1:] #will call all except first, so we can add as many pdf as we want
 
 def pdf_combiner(pdf_list):
+  merger = PyPDF2.PdfFileMerger()
   for pdf in pdf_list:
     print(pdf)
+    merger.append(pdf)
+  merger.write('super.pdf')
 
 pdf_combiner(inputs)
