c6253700152b31c0d36a94adf2c83a910887b4e3
Agnes <brettl1991@gmail.com>
2021-04-15 14:39:11 +0100

    Creating the pdf combiner function and test our code

--- combinePDF.py
+++ combinePDF.py
@@ -3,3 +3,9 @@ import PyPDF2
 import sys
 
 inputs = sys.argv[1:] #will call all except first, so we can add as many pdf as we want
+
+def pdf_combiner(pdf_list):
+  for pdf in pdf_list:
+    print(pdf)
+
+pdf_combiner(inputs)
